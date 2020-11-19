4992d1bd401a015c68560e43cc6746e04e97df79
leovenom <leonardt@oputlook.com>
2020-11-19 15:20:16 +0100

    home, new, edit and delete

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride  = require('method-override');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -20,21 +21,52 @@ const app = express();
 
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))  
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
 
 app.get('/', (req,res) => {
-  res.render('home')
-})
+  res.render('home');
+});
 
 app.get('/campgrounds', async (req, res) => {
   const campgrounds = await Campground.find({});
-  res.render('campgrounds/index', { campgrounds })
+  res.render('campgrounds/index', { campgrounds });
+});
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
-  res.render('campgrounds/show')
+  const campground = await Campground.findById(req.params.id)
+  res.render('campgrounds/show', { campground });
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const campground = await Campground.findById(req.params.id)
+  res.render('campgrounds/edit', { campground });
+});
+
+app.put('/campgrounds/:id', async (req,res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground });
+  res.redirect(`/campgrounds/${campground._id}`);
 })
 
+app.delete('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect('/campgrounds');
+})
 
 app.listen(3000, () => {
   console.log('Serving on portal 3000')
