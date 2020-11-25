a594e94e46f9976e4fa23f448bab8ebc25a9fb6c
leovenom <leonardt@oputlook.com>
2020-11-25 15:38:27 +0100

    routes

---
+++ routes/reviews.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true});
+const catchAsync = require('../utils/catchAsync');
+const { reviewShema } = require('../schemas.js')
+const ExpressError = require('../utils/ExpressError');
+const Campground = require('../models/campground');
+const Review = require('../models/review');
+
+const validateReview = (req, res, next) => {
+  const { error } = reviewShema.validate(req.body);
+  if(error){
+    const msg = error.details.map(el => el.message).join(',')   
+    throw new ExpressError(msg, 400)
+  } else {
+    next();
+  }
+
+}
+
+router.post('/', validateReview, catchAsync(async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  const review = new Review(req.body.review);
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`)
+}));
+
+router.delete('/:reviewId', catchAsync(async (req, res) => {
+  const { id, reviewId } = req.params;
+  await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId} });
+  await Review.findByIdAndDelete(reviewId);
+  res.redirect(`/campgrounds/${id}`);
+}));
+
+module.exports = router;
