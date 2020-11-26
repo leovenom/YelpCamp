88c7bf062f81d6b22b2fac334816b934e129f704
leovenom <leonardt@oputlook.com>
2020-11-26 15:52:02 +0100

    login

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express  = require('express');
 const router =  express.Router();
+const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
@@ -19,6 +20,15 @@ router.post('/register', catchAsync(async (req, res) => {
     res.redirect('register');
   }
 }));
+
+router.get('/login', (req, res) => {
+  res.render('users/login');
+});
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+  req.flash('success', 'welcome back');
+  res.redirect('/campgrounds');
+});
 
 
 module.exports = router;
