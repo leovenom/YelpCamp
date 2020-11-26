43a38bdc03fc5dd33b0d6df6f25e7222cc73099d
leovenom <leonardt@oputlook.com>
2020-11-26 18:25:40 +0100

    delete and review

--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000)
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: '5fbfc085aa820328ed93e259',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: 'https://source.unsplash.com/collection/483251',
