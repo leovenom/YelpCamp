5683e78b3250bf71920f4d81eef15cece58d0d37
leovenom <leonardt@oputlook.com>
2020-11-26 16:32:45 +0100

    currentUser

--- app.js
+++ app.js
@@ -62,6 +62,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
   next();
