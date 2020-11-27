585b9723ac4c9e758a5d9a3fc80a06d7837b5f3c
leovenom <leonardt@oputlook.com>
2020-11-27 20:57:17 +0100

    upload image

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -10,11 +10,11 @@ module.exports.renderNewForm = (req, res) => {
 }
 
 module.exports.createCampground = async (req, res, next) => {
-  // if(!req.body.campground) throw new ExpressError('Invalid campground data', 400);
   const campground = new Campground(req.body.campground);
+  campground.images = req.files.map(f => ({ url: f.path, filename: f.filename}));
   campground.author = req.user._id;
   await campground.save();
-  req.flash('success', 'Successfully made a new campground!')
+  req.flash('success', 'Successfully made a new campground!');
   res.redirect(`/campgrounds/${campground._id}`);
 }
 
--- models/campground.js
+++ models/campground.js
@@ -1,10 +1,16 @@
+const { string } = require('joi');
 const mongoose = require('mongoose');
 const Review = require('./review')
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
     title: String,
-    image: String,
+    images: [
+        {
+            url: String,
+            filename: String
+        }
+    ],
     price: Number,
     description: String,
     location: String,
