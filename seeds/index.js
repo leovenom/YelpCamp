260354df2a9660aecf23ab2a97eb9f4395a61596
leovenom <leonardt@oputlook.com>
2020-11-30 19:45:24 +0100

    added cluster map index

--- seeds/index.js
+++ seeds/index.js
@@ -32,7 +32,10 @@ const seedDB = async () => {
       price,
       geometry: {
         type: "Point",
-        coordinates: [-113.1331, 47.0202]
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude
+        ]
       },
       images:[
         {
