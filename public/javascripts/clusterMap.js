125c7293f0fa569243ea54cd441e4b247231b6ef
leovenom <leonardt@oputlook.com>
2020-11-30 20:04:10 +0100

    added cluster map index seeds connect

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -14,8 +14,7 @@ mapboxgl.accessToken = mapToken;
           type: 'geojson',
           // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
           // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-          data:
-              'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+          data: campgrounds,
           cluster: true,
           clusterMaxZoom: 14, // Max zoom to cluster points on
           clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
