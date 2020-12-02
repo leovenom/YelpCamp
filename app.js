a1ec196abd0cbe2f5a8b99641e0e23a2c77d1f19
leovenom <leonardt@oputlook.com>
2020-12-02 12:21:59 +0100

    testing

--- app.js
+++ app.js
@@ -159,6 +159,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 });
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
   console.log('Serving on portal 3000')
 });
