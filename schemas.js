33527568acaece66781a109c2193d6d51af08bde
leovenom <leonardt@outlook.com>
2023-06-04 21:33:28 +0100

    last

--- schemas.js
+++ schemas.js
@@ -1,8 +1,5 @@
 const BaseJoi = require("joi");
-
-function sanitizeHtml(input) {
-  return input.replace(/(<([^>]+)>)/gi, "");
-}
+const sanitizeHtml = require("sanitize-html");
 
 const extension = (joi) => ({
   type: "string",
@@ -13,7 +10,10 @@ const extension = (joi) => ({
   rules: {
     escapeHTML: {
       validate(value, helpers) {
-        const clean = sanitizeHtml(value);
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
         if (clean !== value)
           return helpers.error("string.escapeHTML", { value });
         return clean;
@@ -28,16 +28,15 @@ module.exports.campgroundSchema = Joi.object({
   campground: Joi.object({
     title: Joi.string().required().escapeHTML(),
     price: Joi.number().required().min(0),
-    // images: Joi.string().required(),
     location: Joi.string().required().escapeHTML(),
     description: Joi.string().required().escapeHTML(),
   }).required(),
   deleteImages: Joi.array(),
 });
 
-module.exports.reviewShema = Joi.object({
+module.exports.reviewSchema = Joi.object({
   review: Joi.object({
-    rating: Joi.number().required().min(0).max(5),
+    rating: Joi.number().required().min(1).max(5),
     body: Joi.string().required().escapeHTML(),
   }).required(),
 });
