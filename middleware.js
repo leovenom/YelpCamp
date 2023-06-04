6288af9507cbacdbb61829c10bdcf91197ebdc73
leovenom <leonardt@outlook.com>
2023-06-04 20:07:36 +0100

    h

--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
-const { campgroundSchema, reviewSchema } = require("./schemas.js");
+const { campgroundSchema, reviewShema } = require("./schemas.js");
 const ExpressError = require("./utils/ExpressError");
 const Campground = require("./models/campground");
 const Review = require("./models/review");
@@ -14,7 +14,6 @@ module.exports.isLoggedIn = (req, res, next) => {
 
 module.exports.validateCampground = (req, res, next) => {
   const { error } = campgroundSchema.validate(req.body);
-  console.log(req.body);
   if (error) {
     const msg = error.details.map((el) => el.message).join(",");
     throw new ExpressError(msg, 400);
@@ -27,7 +26,7 @@ module.exports.isAuthor = async (req, res, next) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
   if (!campground.author.equals(req.user._id)) {
-    req.flash("error", "You do not have permission to do that!");
+    req.flash("error", "You do not have permission to do that");
     return res.redirect(`/campgrounds/${id}`);
   }
   next();
@@ -37,14 +36,14 @@ module.exports.isReviewAuthor = async (req, res, next) => {
   const { id, reviewId } = req.params;
   const review = await Review.findById(reviewId);
   if (!review.author.equals(req.user._id)) {
-    req.flash("error", "You do not have permission to do that!");
+    req.flash("error", "You do not have permission to do that");
     return res.redirect(`/campgrounds/${id}`);
   }
   next();
 };
 
 module.exports.validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
+  const { error } = reviewShema.validate(req.body);
   if (error) {
     const msg = error.details.map((el) => el.message).join(",");
     throw new ExpressError(msg, 400);
