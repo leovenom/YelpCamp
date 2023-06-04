2f818497c95e5fa4ff2f4c23855496e6d91c07b1
leovenom <leonardt@outlook.com>
2023-06-04 17:50:02 +0100

    v

--- schemas.js
+++ schemas.js
@@ -1,10 +1,6 @@
 const BaseJoi = require("joi");
 const sanitizeHtml = require("sanitize-html");
 
-function sanitizeHtml(input) {
-  return input.replace(/(<([^>]+)>)/gi, "");
-}
-
 const extension = (joi) => ({
   type: "string",
   base: joi.string(),
